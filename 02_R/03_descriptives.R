9ecd3a6c8e2940b472b771b3eb65069bb471550d
Paloma Rojas-Saunero <palolili@gmail.com>
2021-07-13 14:51:43 +0200

    its been a while (new edits to Sonja's edits)

--- 02_R/03_descriptives.R
+++ 02_R/03_descriptives.R
@@ -31,7 +31,7 @@ total_n <- dim(data_wide)[1]
 total_cancer <- data_wide %>% 
   count(cancer_v) %>% 
   mutate(prop = round(100*n/sum(n),0)) %>% 
-  mutate(cancer_count = paste0(prop, "%", " (n = ", n, ")")) %>% 
+  mutate(cancer_count = paste0(prop, "%", " (n=", n,")")) %>% 
   select(cancer_count, cancer_v)
   
 no_cancer <- total_cancer %>% 
@@ -46,7 +46,7 @@ total_status <- data_wide %>%
   group_by(cancer_v) %>% 
   count(status) %>% 
   mutate(prop = round(100*n/sum(n),0)) %>% 
-  mutate(total = paste0(prop, "%", " (n = ", n, ")")) %>% 
+  mutate(total = paste0(prop, "%", " (n =", n,")")) %>% 
   select(cancer_v, status, total)
 
 cancer_dem <- total_status %>% filter(cancer_v == 1, status == "Cancer and Dementia") %>% pull(3)
@@ -62,12 +62,12 @@ cancer_time <- data_long %>%
   summarize(m = median(age_scale),
             l = quantile(age_scale, 0.25),
             u = quantile(age_scale, 0.75)) %>% 
-  mutate(temp = paste0(m, " (IQR: ", l, "-", u, ")")) %>% 
+  mutate(temp = paste0(m, " (IQR:", l, "-", u,")")) %>% 
   pull(temp)
 
 dem_total <- data_wide %>% count(outcome_plr) %>% 
   mutate(prop = round(100*n/sum(n),0)) %>% 
-  mutate(temp = paste0(prop, "%", " (n = ", n, ")")) %>%
+  mutate(temp = paste0(prop, "%", " (n=", n,")")) %>%
   filter(outcome_plr == 1) %>% 
   pull(temp)
   
@@ -76,23 +76,23 @@ dem_time <- data_long %>%
   summarize(m = median(age_scale),
             l = quantile(age_scale, 0.25),
             u = quantile(age_scale, 0.75)) %>% 
-  mutate(temp = paste0(m, " (IQR: ", l, "-", u, ")")) %>% 
+  mutate(temp = paste0(m, " (IQR:", l, "-", u,")")) %>% 
   pull(temp)
 
 
 ## Baseline covs
 
 mean_age <- data_wide %>% 
-  summarize(a = round(mean(age_0),  2),
-            b = round(sd(age_0),  2)) %>%
-  mutate(temp = paste0(a, " (SD: ", b, ")")) %>% 
+  summarize(temp = round(mean(age_0),  2)) %>% 
+  #           b = round(sd(age_0),  2)) %>%
+  # mutate(temp = paste0(a, " (SD:", b, ")")) %>% 
   pull(temp)
 
 
 women_prop <- data_wide %>%
   count(sex) %>% 
   mutate(prop = round(100*n/sum(n),0)) %>% 
-  mutate(total = paste0(prop, "%", " (n = ", n, ")")) %>% 
+  mutate(total = paste0(prop, "%", " (n=", n,")")) %>% 
   filter(sex == 1) %>% 
   pull(total)
 
@@ -121,16 +121,23 @@ data_tableone %<>%
            smoke1 == 1 ~ "Former",
            smoke1 == 2 ~ "Current"),
          ht1 = ifelse(ht1 == 1, "History of hypertension", "No history of hypertension"),
+         ht1 = as_factor(ht1),
          hd_prev = case_when(hd_prev == 1 ~ "History of heart disease",
                              hd_prev == 0 ~ "No history of heart disease"),
+         hd_prev = as_factor(hd_prev),
          hd_v = case_when(hd_v == 1 ~ "Incident heart disease",
                           hd_v == 0 ~ "No incident heart disease"),
+         hd_v = as_factor(hd_v),
          diabetes_prev = case_when(diabetes_prev == 1 ~ "History of diabetes",
                                    diabetes_prev == 0 ~ "No history of diabetes",
                                    diabetes_prev == 2 ~ "Unknown"),
+         diabetes_prev = as_factor(diabetes_prev),
          diab_v = ifelse(diab_v == 1, "Incident diabetes", "No incident diabetes"),
+         diab_v = fct_rev(as_factor(diab_v)),
          stroke_prev = ifelse(stroke_prev == 1, "History of stroke", "No history of stroke"),
+         stroke_prev = as_factor(stroke_prev),
          stroke_v = ifelse(stroke_v == 1, "Incident stroke", "No incident stroke"),
+         stroke_v = as_factor(stroke_v),
          cancer_v = ifelse(cancer_v == 1, "Incident cancer", "No incident cancer"))
 
 
@@ -141,32 +148,38 @@ cat <- myvars[!myvars %in% num]
 
 tableone <- CreateTableOne(vars = myvars, data = data_tableone, factorVars = cat, strata = "cancer_v")
 
-# summary(tableone)
 
-# survminer::ggcompetingrisks(t2cancer_km) +
-#   ggthemes::scale_fill_tableau() +
-#   theme_minimal() +
-#   theme(
-#     legend.position = "bottom")
+tableone_exp <-
+  print(
+    tableone,
+    explain = TRUE,
+    test = FALSE,
+    quote = FALSE,
+    noSpaces = TRUE,
+    printToggle = FALSE,
+  )
 
-## Probability of cancer and dementia at 5 years
-data_wide <- data_wide %>% 
-  mutate(cancer_death = ifelse(cancer_v == 0 & competing_plr == 1, 2, cancer_v))
+Characteristics <- rownames(tableone_exp)
 
-t2cancer_km <-
-  survfit(Surv(t2cancer_y, as.factor(cancer_death)) ~ 1, data_wide)
-
-
-cancer_death_5y <- t2cancer_km %>% 
-  broom::tidy() %>% 
-  filter(state %in% c(1,2)) %>% 
-  filter(between(time, 5.00, 5.01)) %>% 
-  select(estimate, conf.low, conf.high, state) %>% 
-  mutate_if(is.numeric, ~.*100) %>% 
-  mutate_if(is.numeric, round, 1) %>%  
-  mutate(est_print = paste0(estimate, " (95%CI: ", conf.low, ", ", conf.high, ")")) %>% 
-  select(est_print, state)
-
-death_5y <- cancer_death_5y %>% filter(state == 2) %>% pull(1)
-cancer_5y <- cancer_death_5y %>% filter(state == 1) %>% pull(1)
-
+tableone_complete <- tableone_exp %>% as_tibble() %>%
+  cbind(Characteristics) %>% select(Characteristics, everything()) %>%
+  mutate(
+    Characteristics = str_replace(Characteristics, "smoke1", "Smoking status"),
+    Characteristics = str_replace(Characteristics, "sex = ", ""),
+    Characteristics = str_replace(Characteristics, "age_0", "Age at baseline"),
+    Characteristics = str_replace(Characteristics, "education", "Educational attainment"),
+    Characteristics = str_replace(Characteristics, "apoe4", "ApoE4"),
+    Characteristics = str_replace(Characteristics, "bmi1", "Body Mass Index"),
+    Characteristics = str_replace(Characteristics, "sbp1", "Systolic blood pressure (mmHg)"),
+    Characteristics = str_replace(Characteristics, "ht1 = ", ""),
+    Characteristics = str_replace(Characteristics, "hd_prev = ", ""),
+    Characteristics = str_replace(Characteristics, "hd_v = ", ""),
+    Characteristics = str_replace(Characteristics, "diab_v = ", ""),
+    Characteristics = str_replace(Characteristics, "Unknown", "Unknown history of diabetes"),
+    Characteristics = str_replace(Characteristics, "stroke_prev = ", ""),
+    Characteristics = str_replace(Characteristics, "stroke_v = ", ""),
+  ) %>% 
+  filter(!Characteristics %in% c("oh1 (mean (SD))", "diabetes_prev (%)", "cancer_v = No incident cancer (%)")) %>% 
+  filter(!Characteristics %in% str_detect(Characteristics, "No history of diabetes")) %>%  ## Not working :S
+  rename(`Incident cancer during follow-up` = `Incident cancer`,
+         `No incident cancer observed during follow-up` = `No incident cancer`)
--- 02_R/table_2.R
+++ 02_R/table_2.R
@@ -6,7 +6,7 @@ hr <-rio::import(here::here("02_R", "hr_results.csv")) %>%
   mutate(Proxy = ifelse(Proxy == "Time to cancer", "Time-varying", Proxy)) %>% 
   mutate_if(is.numeric, round, 2) %>% 
   mutate(Proxy = str_replace(Proxy, "Ever vs. Never", "Ever vs. never")) %>% 
-  mutate(`Hazard Ratio` = paste0(point, "(", conf.low, ",", conf.high, ")")) %>% 
+  mutate(`Hazard Ratio` = paste0(point, " (", conf.low, ",", conf.high, ")")) %>% 
   select(Proxy, model, `Hazard Ratio`)
 
 rr <- ever %>%
@@ -35,9 +35,15 @@ rd <- ever %>%
 complete_table <- rd %>% left_join(rr) %>% left_join(hr)
 
 complete_table <- complete_table %>% 
-  mutate(model = as_factor(model),
-         model = fct_relevel(model, c("Unadjusted", "IPTW", "IPTW + IPCW"))) %>% 
-  group_by(Proxy) %>% 
+  mutate(Proxy = ifelse(Proxy == "Ever vs. never", "A", "B")) %>%
+  rename(Scenario = Proxy) %>% 
+  mutate(model = 
+           case_when(model == "IPTW" ~ "With weights for confounding",
+                     model == "IPTW + IPCW" ~ "With weights for confounding and for censoring for death",
+                     TRUE ~ model),
+         model = as_factor(model),
+         model = fct_relevel(model, c("Unadjusted", "With weights for confounding", "With weights for confounding and for censoring for death"))) %>% 
+  group_by(Scenario) %>% 
   arrange(model)
 
 bounds <- rio::import(here::here("02_R", "table_results_bounds.csv"))
@@ -45,3 +51,5 @@ bounds <- rio::import(here::here("02_R", "table_results_bounds.csv"))
 bounds_tv_ipw <- bounds %>% slice(4,6) %>% select(model, rr) %>% pull(rr)
 
 bounds_tv_ipw <- paste0("RR: ", bounds_tv_ipw[1], ", ", bounds_tv_ipw[2])
+
+
